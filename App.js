9ec1d59c3b5ef71d27be3835575845bde87c526a
Santhoshini29 <adumalapellisanthoshini29@gmail.com>
2024-04-04 08:26:46 +0530

    2

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
 const parent= ("div",{id:"parent"},[
 React.createElement("div",{id :"child1"},[
- React.createElement("h1",{},"this is h1"),
+ React.createElement("h1",{},"this is h1 from child1"),
  React.createElement("h2",{},"this is h2"),    
 ]),
 React.createElement("div",{id :"child2"},[
